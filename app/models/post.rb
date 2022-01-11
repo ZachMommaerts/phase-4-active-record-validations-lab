24bcdbe8976807aaada6bd4cbdaa51c975cbb10b
Zach <zmommaerts@gmail.com>
2022-01-11 12:15:38 -0600

    stuck

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, comparison: { equal_to: "Fiction" }
+    validates :category, inclusion: { in: ["Fiction", 'Non-Fiction'] }
+
+    def no_clickbait_titles
+        if title == 'True Facts'
+            errors.add(:title, 'No clickbait ya dingus')
+        end
+    end
 end
